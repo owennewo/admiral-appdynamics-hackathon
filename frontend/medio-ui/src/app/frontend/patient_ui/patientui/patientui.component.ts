c38e3ca0c0476d231b6d3674177e3e363e304226
David Plunkett <David.Plunkett@admiralgroup.co.uk>
2019-11-15 07:32:51 +0000

    added some tabs with prescription info

--- frontend/medio-ui/src/app/frontend/patient_ui/patientui/patientui.component.ts
+++ frontend/medio-ui/src/app/frontend/patient_ui/patientui/patientui.component.ts
@@ -8,12 +8,77 @@ import { Prescription } from 'src/app/model/prescription';
 })
 export class PatientuiComponent implements OnInit {
 
-  prescription : Prescription;
+  prescription : Prescription =    { 
+  conditions: [],
+  courseDurationDays: 20,
+  doctorName: "Dr. Bob Smith",
+  doctorNumber: "01945 411411",
+  instructions: ["Take one twice a day", "Put tablet under tongue"],
+  intervalSeconds: 5,
+  medication: ["2x Polo, twice daily", "1x Smarties, 5 times daily"],
   
+  nextPrescriptionSeconds: 10,
+  perDay: 3,
+  quantityRemaining: 4,
+  quantityTotal: 7,
+  sideEffects: ["Contains Laxatives", "Bad side effect"]
+  }
+  
+  //Tabs
+  homeTab: boolean = true;
+  prescriptionTab: boolean = false;
+  infoTab: boolean = false;
+  contactGPTab: boolean = false;
+  aboutTab: boolean = false;
+
 
   constructor() { }
 
   ngOnInit() {
+    
   }
+
+
+  homeButtonClick(event: Event) {
+    this.homeTab = true;
+    this.prescriptionTab = false;
+    this.infoTab = false;
+    this.contactGPTab = false;
+    this.aboutTab = false;
+    
+  }
+
+  prescriptionButtonClick(event: Event) {
+    this.homeTab = false;
+    this.prescriptionTab = true;
+    this.infoTab = false;
+    this.contactGPTab = false;
+    this.aboutTab = false;
+  }
+
+  infoButtonClick(event: Event) {
+    this.homeTab = false;
+    this.prescriptionTab = false;
+    this.infoTab = true;
+    this.contactGPTab = false;
+    this.aboutTab = false;
+  }
+
+  contactButtonClick(event: Event) {
+    this.homeTab = false;
+    this.prescriptionTab = false;
+    this.infoTab = false;
+    this.contactGPTab = true;
+    this.aboutTab = false;
+  }
+
+  aboutButtonClick(event: Event) {
+    this.homeTab = false;
+    this.prescriptionTab = false;
+    this.infoTab = false;
+    this.contactGPTab = false;
+    this.aboutTab = true;
+  }
+
 
 }
